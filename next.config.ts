ae5b242296e3f1a1235cc0ff4c10c4413fa002dc
Joshua Benjamin <joshbenja@gmail.com>
2026-09-13 21:42:39 -0500

    Load static assets from GitHub Pages path

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  assetPrefix: process.env.GITHUB_ACTIONS ? 'https://blarphus.github.io/whisperbook-reader' : '',
 };
 
 export default nextConfig;
